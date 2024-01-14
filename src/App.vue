d56c86627c834c897bf17f61de47026a3ace4275
Oguzcan Karakoc <okarakoc.mp@gmail.com>
2024-01-14 16:03:06 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+</template>
